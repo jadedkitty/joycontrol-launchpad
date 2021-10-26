9aac81b6eabd1c4bddd9ed94bb1b80c56ff6e7a4
Emma Maguire <emma@emmalyndesign.com>
2021-10-26 16:36:58 -0700

    Update and rename amiibo.sh to joycontrol-launchpad.sh

--- joycontrol-launchpad.sh
+++ joycontrol-launchpad.sh
@@ -10,28 +10,6 @@ resetBluetooth() {
     echo "Bluetooth reset."
 }
 
-# submenu
-submenu() {
-    local PS3='Please enter sub option: '
-    local options=("Sub menu item 1" "Sub menu item 2" "Sub menu quit")
-    local opt
-    select opt in "${options[@]}"; do
-        case $opt in
-        "Sub menu item 1")
-            echo "you chose sub item 1"
-            ;;
-        "Sub menu item 2")
-            echo "you chose sub item 2"
-            ;;
-        "Sub menu quit")
-            return
-            ;;
-        *) echo "invalid option $REPLY" ;;
-        esac
-    done
-}
-
-# submenu
 connectSwitch() {
     local PS3='Connect options: '
     local options=("Connect New Switch" "Reconnect Switch" "Load Amiibo" "Go Back")
